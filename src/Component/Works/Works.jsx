169fc4ad5b8e9ffd0fb87d0b3b1b1591ca2ec6ec
Richard Shaju <richardshaju66@gmail.com>
2023-09-22 23:36:02 +0530

    maxcart new link updated

--- src/Component/Works/Works.jsx
+++ src/Component/Works/Works.jsx
@@ -12,21 +12,68 @@ function Works() {
       </div>
       <div className="works-display">
         <div className="item" style={{ textAlign: "center" }}>
-          <img src={require("./olx.png")} alt="" />
+          <img src={require("./ir.png")} alt="" />
           <a
             className="link-icon"
-            href="https://richardshaju.github.io/olx-clone/"
+            href="http://ec2-13-48-45-183.eu-north-1.compute.amazonaws.com/"
             target="_blank"
             rel="noopener noreferrer"
           >
             <div className="item-title cart">
-              <span>Olx-Clone</span>
+              <span>Image Recognizer</span>
               <i className="fa fa-external-link" aria-hidden="true"></i>
             </div>
           </a>
           <p style={{ paddingTop: "0.6rem" }}>
-            Front-end : <b>React JS</b> <br />
-            Back-end : <b>Firebase</b>
+            Front-end : <b>Html, Css, Js</b> <br />
+            Web server : <b>Nginx</b><br />
+            Back-end : <b>Flask</b> <br />
+            Cloud : <b>AWS</b> <br />
+            ML Algorithm : <b>Support Vector Machine</b>
+          </p>
+        </div>
+        <div className="item" style={{ textAlign: "center" }}>
+          <img src={require("./Capture.JPG")} alt="" />
+          <a
+            className="link-icon"
+            href="http://ec2-16-171-239-94.eu-north-1.compute.amazonaws.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="item-title cart">
+              <span>Shopping Website</span>
+              <i className="fa fa-external-link" aria-hidden="true"></i>
+            </div>
+          </a>
+          <p style={{ paddingTop: "0.6rem" }}>
+            Back-end : <b>NodeJS</b> <br />
+            Data Base : <b>MongoDB</b> <br />
+            Front-end : <b>Html,Css,Javascript</b> <br />
+            FrameWorks : <b>Handlebars,ExpressJS,Bootstrap</b>{" "}
+          </p>
+        </div>
+      
+      </div>
+      <div className="works-display" style={{ marginBottom: "2rem" }}>
+        <div className="item" style={{ textAlign: "center" }}>
+          <img src={require("./tic.png")} alt="" />
+          <a
+            className="link-icon"
+            href="https://richardshaju.github.io/tictactoe"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="item-title cart">
+              <span>AI Tic Tac Toe</span>
+              <i className="fa fa-external-link" aria-hidden="true"></i>
+            </div>
+          </a>
+          <p style={{ paddingTop: "0.6rem" }}>
+            FrameWorks : <b>React JS</b>
+            <br />
+            Algorithm: <b>Minimax</b> <br />
+            Feature: <b>You can't win</b> <br />
+            Front-end : <b>Html,Css,Javascript</b> <br />
           </p>
         </div>
         <div className="item" style={{ textAlign: "center" }}>
@@ -45,55 +92,14 @@ function Works() {
           <p style={{ paddingTop: "0.6rem" }}>
             Back-end : <b>Nill</b> <br />
             API used : <b>TMDB</b> <br />
-            FrameWorks : <b>ReactJS</b><br />
+            FrameWorks : <b>ReactJS</b>
+            <br />
             Front-end : <b>React,Css,Javascript</b> <br />
           </p>
         </div>
       </div>
       <div className="works-display" style={{ marginBottom: "2rem" }}>
-      <div className="item" style={{ textAlign: "center" }}>
-          <img src={require("./tic.png")} alt="" />
-          <a
-            className="link-icon"
-            href="https://richardshaju.github.io/tictactoe"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div className="item-title cart">
-              <span>AI Tic Tac Toe</span>
-              <i className="fa fa-external-link" aria-hidden="true"></i>
-            </div>
-          </a>
-          <p style={{ paddingTop: "0.6rem" }}>
-            FrameWorks : <b>React JS</b><br />
-            Algorithm: <b>Minimax</b> <br />
-            Feature: <b>You can't win</b> <br />
-            Front-end : <b>Html,Css,Javascript</b> <br />
-          </p>
-        </div>
         <div className="item" style={{ textAlign: "center" }}>
-          <img src={require("./Capture.JPG")} alt="" />
-          <a
-            className="link-icon"
-            href="https://maxcart.onrender.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div className="item-title cart">
-              <span>Shopping Website</span>
-              <i className="fa fa-external-link" aria-hidden="true"></i>
-            </div>
-          </a>
-          <p style={{ paddingTop: "0.6rem" }}>
-            Back-end : <b>NodeJS</b> <br />
-            Data Base : <b>MongoDB</b> <br />
-            Front-end : <b>Html,Css,Javascript</b> <br />
-            FrameWorks : <b>Handlebars,ExpressJS,Bootstrap</b>{" "}
-          </p>
-        </div>
-      </div>
-      <div className="works-display" style={{ marginBottom: "2rem" }}>
-      <div className="item" style={{ textAlign: "center" }}>
           <img src={require("./weeknd.JPG")} alt="" />
           <a
             className="link-icon"
@@ -110,7 +116,24 @@ function Works() {
             Front-end : <b>Html,Css,Javascript</b>{" "}
           </p>
         </div>
-        
+        <div className="item" style={{ textAlign: "center" }}>
+          <img src={require("./olx.png")} alt="" />
+          <a
+            className="link-icon"
+            href="https://richardshaju.github.io/olx-clone/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="item-title cart">
+              <span>Olx-Clone</span>
+              <i className="fa fa-external-link" aria-hidden="true"></i>
+            </div>
+          </a>
+          <p style={{ paddingTop: "0.6rem" }}>
+            Front-end : <b>React JS</b> <br />
+            Back-end : <b>Firebase</b>
+          </p>
+        </div>
       </div>
     </section>
   );
